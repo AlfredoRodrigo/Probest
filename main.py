f595321b2df02e776dbf72e3c570e5992db96888
Alfredo Rodrigo <alf_rodrigo14@hotmail.com>
2017-08-29 14:57:53 -0300

    Atualizando arquivos

--- main.py
+++ main.py
@@ -1,49 +1,29 @@
 import matplotlib.pyplot as plt
-import pandas as pd
+from situacao import *
 
 df = pd.read_excel('alunos.ifpb.xls')
 
-newData = df[df.columns[[3, 4, 6]]]
-
-afastado = newData[newData.Situação == 'Afastado']
-aguardandoColacao = newData[newData.Situação == 'Aguardando Colação de Grau']
-aguardandoENADE = newData[newData.Situação == 'Aguardando ENADE']
-cancelado = newData[newData.Situação == 'Cancelado']
-cancelamentoCompulsorio = newData[newData.Situação == 'Cancelamento']
-concludente = newData[newData.Situação == 'Concludente']
-concluido = newData[newData.Situação == 'Concluído']
-egresso = newData[newData.Situação == 'Egresso']
-estagiarioConcludente = newData[newData.Situação == 'Estagiario (Concludente)']
-evasao = newData[newData.Situação == 'Evasão']
-falecido = newData[newData.Situação == 'Falecido']
-formado = newData[newData.Situação == 'Formado']
-intercambio = newData[newData.Situação == 'Intercâmbio']
-matriculado = newData[newData.Situação == 'Matriculado']
-trancado = newData[newData.Situação == 'Trancado']
-trancadoVoluntariamente = newData[newData.Situação == 'Trancado Voluntariamente']
-transferidoExterno = newData[newData.Situação == 'Transferido Externo']
-
 situacao = pd.crosstab(index=newData["Situação"], columns="Quantidade")
+curso = pd.crosstab(index=newData["Curso"], columns="Quantidade")
+periodo = pd.crosstab(index=newData["Ano/Periodo Letivo"], columns="Quantidade")
 
-porcentagem = situacao/situacao.sum()
+evadidosPorCurso = pd.crosstab(index=evasao["Curso"], columns="Quantidade")
+
+print(evadidosPorCurso)
+
+porcentagem = evadidosPorCurso/evadidosPorCurso.sum()
 print(porcentagem, "\n")
 
-# print(porcentagem[porcentagem.columns[0]].ix[0])
-
-# a = situacao.rownames
-# print(a)
-
 sizes = []
-for indice in range(17):
+for indice in range(12):
     sizes.append(porcentagem[porcentagem.columns[0]].ix[indice])
 
-# Pie chart, where the slices will be ordered and plotted counter-clockwise:
-labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
-explode = (0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.1, 0.5, 0.5, 0.5)  # only "explode" the 2nd slice (i.e. 'Hogs')
+labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
 
 fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
         shadow=False, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+ax1.axis('equal')
 
 plt.show()
