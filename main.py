b05a80e5aee8909fb3bff1bdc9219c5672f14a5e
Alfredo Rodrigo <alf_rodrigo14@hotmail.com>
2017-09-14 19:30:55 -0300

    Inserindo arquivos mais organizados e com mais funcionalidades

--- main.py
+++ main.py
@@ -1,29 +1,14 @@
-import matplotlib.pyplot as plt
-from situacao import *
+# TODAS AS FUNÇÕES IMPLEMENTADAS EM "GRAFICOS.PY" RODAM AQUI
+# OS NOMES DAS FUNÇÕES SÃO EXTREMAMENTE SUGESTIVOS
+# QTD = QUANTIDADE; PCT = PORCENTAGEM
 
-df = pd.read_excel('alunos.ifpb.xls')
+import graficos as gf
 
-situacao = pd.crosstab(index=newData["Situação"], columns="Quantidade")
-curso = pd.crosstab(index=newData["Curso"], columns="Quantidade")
-periodo = pd.crosstab(index=newData["Ano/Periodo Letivo"], columns="Quantidade")
-
-evadidosPorCurso = pd.crosstab(index=evasao["Curso"], columns="Quantidade")
-
-print(evadidosPorCurso)
-
-porcentagem = evadidosPorCurso/evadidosPorCurso.sum()
-print(porcentagem, "\n")
-
-sizes = []
-for indice in range(12):
-    sizes.append(porcentagem[porcentagem.columns[0]].ix[indice])
-
-labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-
-
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
-        shadow=False, startangle=90)
-ax1.axis('equal')
-
-plt.show()
+gf.qtd_situacao_com_matriculados()
+gf.pct_situacao_com_matriculados()
+gf.qtd_situacao_sem_matriculados()
+gf.pct_situacao_sem_matriculados()
+gf.qtd_matriculados_por_curso()
+gf.pct_matriculados_por_curso()
+gf.quantidade_evadidos_por_curso()
+gf.porcentagem_evadidos_por_curso()
